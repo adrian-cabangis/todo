4bdcce4b7fae86c805ab5f395845796950a18d7a
adrian-cabangis <adrian.cabangis@logiclabph.com>
2026-02-12 10:29:00 +0800

    Updated dynamic url for user-specific task page

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Link } from '@inertiajs/vue3';
+import { Link, usePage } from '@inertiajs/vue3';
 import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
 import NavFooter from '@/components/NavFooter.vue';
 import NavMain from '@/components/NavMain.vue';
@@ -14,36 +14,38 @@ import {
     SidebarMenuItem,
 } from '@/components/ui/sidebar';
 import { dashboard } from '@/routes';
-import { User, type NavItem } from '@/types';
+import { type NavItem } from '@/types';
 import AppLogo from './AppLogo.vue';
+import { computed } from 'vue';
 
-const prop = defineProps<{
-    user: User[];
-}>();
+const page = usePage();
 
-const mainNavItems: NavItem[] = [
-    {
-        title: 'Dashboard',
-        href: dashboard(),
-        icon: LayoutGrid,
-    },
-    {
-        title: 'About',
-        href: '/about',
-        icon: LayoutGrid,
-    },
-    {
-        title: 'All Tasks',
-        href: '/tasks',
-        icon: LayoutGrid,
-    },
-    {
-        title: 'My Tasks',
-        href: `/tasks/2`,
-        icon: LayoutGrid,
-    },
-];
+const mainNavItems = computed<NavItem[]>(() => {
+    const user = page.props.auth.user;
 
+    return [
+        {
+            title: 'Dashboard',
+            href: dashboard(),
+            icon: LayoutGrid,
+        },
+        {
+            title: 'About',
+            href: '/about',
+            icon: LayoutGrid,
+        },
+        {
+            title: 'All Tasks',
+            href: '/tasks',
+            icon: LayoutGrid,
+        },
+        {
+            title: 'My Tasks',
+            href: `/tasks/${user.id}`,
+            icon: LayoutGrid,
+        },
+    ];
+});
 const footerNavItems: NavItem[] = [
     {
         title: 'Github Repo',
